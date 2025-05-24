2ed38ed682ed67fc8dc51d633e4183b4e98a0739
Tatiana Fiore <tatianafiore@gmail.com>
2025-05-24 12:13:28 -0300

    Game Seed

--- src/main/java/com/fiore/dslist/entities/Game.java
+++ src/main/java/com/fiore/dslist/entities/Game.java
@@ -22,25 +22,30 @@ public class Game {
 	private Integer year; 
 	private String genre;
 	private String platforms; 
-	private String imgUrl; 
-	private String shortDescription;
-	private String longDescription; 
 	private Double score; 
+	private String imgUrl; 
 	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription; 
+			
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
-			String shortDescription, String longDescription, Double score) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
-		this.score = score;
+		
 	}
 
 	public Long getId() {
@@ -82,6 +87,14 @@ public class Game {
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
 	}
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
 
 	public String getImgUrl() {
 		return imgUrl;
@@ -107,13 +120,7 @@ public class Game {
 		this.longDescription = longDescription;
 	}
 
-	public Double getScore() {
-		return score;
-	}
-
-	public void setScore(Double score) {
-		this.score = score;
-	}
+	
 
 	@Override
 	public int hashCode() {
