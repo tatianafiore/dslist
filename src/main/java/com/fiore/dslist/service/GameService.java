4e703e6432c6fa109aaf00c11c3d86903146f86b
Tatiana Fiore <tatianafiore@gmail.com>
2025-05-24 23:21:19 -0300

    GET game by id

--- src/main/java/com/fiore/dslist/service/GameService.java
+++ src/main/java/com/fiore/dslist/service/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.fiore.dslist.dto.GameDTO;
 import com.fiore.dslist.dto.GameMinDTO;
 import com.fiore.dslist.entities.Game;
 import com.fiore.dslist.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true) //não vou fazer nenhuma operação de escrita. Não vou bloquear o banco de dados para escrita
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get(); 
+		return new GameDTO(result);
+
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO (x)).toList();
